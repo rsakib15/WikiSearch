84dc748c925010c18803cac799e4eca62c4f31dc
Sakib Rahman <srskb15@gmail.com>
2021-05-29 11:54:51 +0800

    SA | frontend index.js file added

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import reducer, { initialState } from "./store/reducer";
+import { StateProvider } from "./store/StateProvider";
+import "./styles/index.css";
+
+ReactDOM.render(
+    <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+    </StateProvider>,
+    document.getElementById("root")
+);
